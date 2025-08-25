d0a2cf4a574e83bb0f56ab643664aa16e1a632c3
jiyeeun <opc3856@gmail.com>
2025-08-25 09:28:26 +0900

    Add initial files

---
+++ Untitled2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age, score; //정수 변수 선언문
+	 
+	
+	printf("나이가 어떻게 되세요? : ", age); scanf("%d", &age);
+	printf("점수는요? : ", score); scanf("%d", &score);
+	
+	printf("\nMy age : %d \n", age);
+	printf("%d is my point \n", score);
+	printf("Good \nmorning \neverybody\n"); 
+	//printf("My age : %d \n%d is my point \nGood \nmorning \neverybody\n",20,100);
+	
+	return 0;
+
+}
+
+
+
